931962dde6bada1775069cbe82cc1e990ad64a88
lampiosko <claudio.abele@gmail.com>
2021-05-18 13:13:59 -0400

    getBook API

--- src/java/API/BookResource.java
+++ src/java/API/BookResource.java
@@ -7,6 +7,7 @@ package API;
 
 import Class.Libro;
 import Dao.LibroDao;
+import com.google.gson.Gson;
 import com.sun.management.jmx.ServiceName;
 import java.sql.SQLException;
 import javax.ws.rs.core.Context;
@@ -19,6 +20,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -48,6 +50,22 @@ public class BookResource {
         String json = "\"id\":\""+ dao.getLastId() +"\"";
         return Response.ok(json, MediaType.APPLICATION_JSON).build();
      
+    }
+    
+    @GET
+    @Path("{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getBook(@PathParam("id") int id){
+        try{
+            LibroDao dao = new LibroDao();
+            Libro libro = dao.getBook(id);
+            if(libro == null) return Response.status(Response.Status.NOT_FOUND).entity("El libro no existe").build();
+            String json = new Gson().toJson(libro);
+            return Response.ok(json, MediaType.APPLICATION_JSON).build();
+        }catch(Exception ex){
+             return Response.status(Response.Status.SEE_OTHER).entity("Error: " + ex.toString()).build();
+        }
     }
 
     
--- src/java/Dao/LibroDao.java
+++ src/java/Dao/LibroDao.java
@@ -16,15 +16,15 @@ import java.sql.SQLException;
  * @author Diego
  */
 public class LibroDao {
-    
-    public LibroDao(){
-        
+
+    public LibroDao() {
+
     }
-    
-    public String create(Libro libro) throws SQLException{
+
+    public String create(Libro libro) throws SQLException {
         Connection conn = Dao.getConnection();
-        
-        try{
+
+        try {
             String sql = "insert into libro(isbn,titulo,editorial,anno,autor1,autor2,precio) values (?,?,?,?,?,?,?)";
             PreparedStatement ps = conn.prepareStatement(sql);
             ps.setString(1, libro.getIsbn());
@@ -34,35 +34,64 @@ public class LibroDao {
             ps.setString(5, libro.getAutor1());
             ps.setString(6, libro.getAutor2());
             ps.setInt(7, libro.getPrecio());
-            
+
             int rs = ps.executeUpdate();
             System.out.println(rs);
-            
+
             conn.close();
-            
+
             return "";
-            
-        }catch(SQLException sqlEx){
+
+        } catch (SQLException sqlEx) {
             System.out.println(sqlEx.getMessage());
             conn.close();
             return sqlEx.getMessage();
         }
         //PreparedStatement
     }
-    
-    public int getLastId(){
+
+    public int getLastId() {
         Connection conn = Dao.getConnection();
         int res = -1;
-        try{
+        try {
             String sql = "select max(id) from libro";
             PreparedStatement ps = conn.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
             res = rs.getInt(1);
             conn.close();
-            
-        }catch(SQLException sqlEx){
+
+        } catch (SQLException sqlEx) {
             System.out.println(sqlEx.getMessage());
         }
         return res;
     }
+
+    public Libro getBook(int id) {
+
+        Connection conn = Dao.getConnection();
+        String sql = "select * from libro where id = ?";
+
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            Libro libro = new Libro(
+                    rs.getString("isbn"),
+                    rs.getString("titulo"),
+                    rs.getString("editorial"),
+                    rs.getString("anno"),
+                    rs.getString("autor1"),
+                    rs.getString("autor2"),
+                    rs.getInt("precio"));
+            libro.setId(rs.getInt("id"));
+            conn.close();
+            return libro;
+
+        } catch (SQLException sqlEx) {
+            System.out.println(sqlEx.getMessage());
+            return null;
+        }
+    }
 }
+
+
